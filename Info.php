fea8629a618e06d53134934b95435a42d8f35554
Rob Korobkin <rob.korobkin@gmail.com>
2018-05-06 18:20:38 -0400

    Updated info page.

--- Info.php
+++ Info.php
@@ -13,41 +13,80 @@
 		<h1 class="title">Info</h1>
 	</div>
 
+	<p class="heading" style="font-weight: bold">Welcome to JobQuery.org a new product from Mountain Bunny!</p>	
+
+
 	<p class="heading">Why we Made This App</p>
 	<div class="text">
 		<p>Lots of teenagers in Maine want jobs. They want to make money that’s theirs to spend, and if their parents won’t pay for everything, teenagers might also want to save up for the future. While teenagers can’t make the same kind of time commitments that adults do, they can still do all sorts of things, including mow lawns, shovel snow, wash dishes and work entry level retail, just to name a few examples.</p> 
 
-			<p>The problem is that it can be hard to understand how to go about finding and starting a job because teenagers are too young for many of the jobs that get officially listed online, and the application and permitting process can also be very confusing. Maine’s teenagers need help finding opportunities and navigating that process. </p>
+		<p>The problem is that it can be hard to understand how to go about finding and starting a job because teenagers are too young for many of the jobs that get officially listed online, and the application and permitting process can also be very confusing. Maine’s teenagers need help finding opportunities and navigating that process. </p>
 	</div>
 
 	<p class="heading"> How to Use the App </p>
+
 	<div class="text">
-		<p> Along the footer, you will see four buttons HOME, WORK PERMIT APPLICATION, ABOUT, and PROFILE.</p>
+		<p> Along the footer, you will see four buttons HOME, EMPLOYERS and INFO.</p>
     
-	<p>The HOME screen is all of the available jobs. It is a list of jobs and locations. All jobs are seeking people from 14-20 years old. You can also filter the job search in the header.  Tap the  magnifying glass to filter searches. There is a back button that looks like some text followed by downward facing arrow at the top of the home page. If you tap on it you can see recent searches and select them. </p> 
+		<p>The HOME screen is all of the available jobs. It is designed to be a list of jobs seeking people from 14-20 years old. At the top you will find the job that has been marked as "sponsored" in our database.</p> 
 
-	<p>WORK PERMIT APPLICATION is a link to a work permit you can print and fill out.</p>
+		<p>The EMPLOYERS page is designed to be a list of local employers seeking to hire young people. When the user clicks on the name of an employer, the see the list of currently being offered by that employer.</p>
 
-	<p>ABOUT page is where you are currently. This page outlines the apps use and tells you a little about the developers. It also provides links to important information regarding the project and the idea the sparked it.</p>
-
-	<p>PROFILE is for business to post available jobs along with specifications. The profiles will include age, description of job, picture, name, and social media and contact information. This is not a completed section we are still working to improve both the mobile and desktop versions.</p>
+		<p>The ABOUT page is where you are currently. This page outlines the app's use and tells you a little about the developers. It also provides links to important information regarding the project and the idea the sparked it.</p>
 
 	</div>
  
 	<p class="heading"> Special Thanks<p>
 	<div class="text">
-	<p>A special thanks to Rob Korobkin for advising our group this year. Rob is a local web developer and community organizer. Thank you, Rob, for teaching us to code and design software. </p>
-	<p>Also a big thank you to Halsted Larsson for overseeing the computer science cluster on Fridays.</p>
-	<p>Icons made by Smashicons from www.flaticon.com </p>
+		<p>A special thanks to Rob Korobkin for advising our group this year. Rob is a local web developer and community organizer. Thank you, Rob, for teaching us to code and design software. </p>
+		<p>Also a big thank you to Halsted Larsson for overseeing the computer science cluster on Fridays.</p>
+		<p>Icons made by Smashicons from www.flaticon.com </p>
 
 	</div>
+
+	<p class="heading"> Contact</p>
+	<div class="text">
+		<p><b>Fred Czerwinski</b>
+		<br />frederick.czerwinski@baxter-academy.org 
+	</div>
+
 
 	<p class="heading"> Developers</p>
 	<div class="text">
-		<p>Mustafa Abu Hamad: 9th Grade</p>
-		<p>Fred Czerwinski: 9th Grade</p>
-		<p>Laura Morris: 9th Grade</p>
+		<p>All of us are Freshman at the Baxter Academy for Technology and Science in Portland, Me.</p>
+
+		<p><b>Mustafa Abu Hamad</b><br />Full Stack Developer<br />9th Grade</p>
+		<p><b>Fred Czerwinski</b><br />UI / UX<br />9th Grade</p>
+		<p><b>Laura Morris</b><br />Project Management<br />9th Grade</p>
 	</div>
+
+
+	<p class="heading">Links</p>
+	<div class="text">
+		<p><b>Check out our app here:</b>
+			<br /><a href="http://m.jobquery.org" target="_blank">http://m.jobquery.org</a>
+		</p>
+		<p>
+			<b>Watch our video here:</b>
+			<br /><a href="https://youtu.be/7xby6IHSBqo" target="_blank">https://youtu.be/7xby6IHSBqo</a>
+		</p>
+
+		<p>
+			<b>The code for this submission can be found on GitHub here:</b>
+			<br /><a href="https://github.com/Baxter-FIndAJob/mobile-jobquery">https://github.com/Baxter-FIndAJob/mobile-jobquery</a>
+		</p>
+
+
+		<p>
+			<b>Our master repository for ongoing development, including our desktop interface and user management system, is here:</b>
+			<br /><a href="https://github.com/Baxter-FIndAJob/Find-A-Job">https://github.com/Baxter-FIndAJob/Find-A-Job</a>
+		</p>
+	</div>
+
+
+	<p class="heading">Thank you for your consideration!</p>
+
+
 
 
 	<?php require('_includes/footer.php');?>	
