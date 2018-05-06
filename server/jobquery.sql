4c457869d856c728da6f122133c4756c281fafc7
Rob Korobkin <rob.korobkin@gmail.com>
2018-05-06 17:35:30 -0400

    moving sql (simplifying).

--- server/jobquery.sql
+++ server/jobquery.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 13, 2018 at 03:12 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
+-- Generation Time: May 06, 2018 at 09:31 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `baxter_jobquery`
@@ -34,18 +26,20 @@ CREATE TABLE `employers` (
   `employerDescription` varchar(255) NOT NULL,
   `employerLocation` varchar(255) NOT NULL,
   `employerType` varchar(255) NOT NULL,
-  `employerSponsor` varchar(255) NOT NULL
+  `employerSponsor` varchar(255) NOT NULL,
+  `Picture` varchar(255) NOT NULL,
+  `test` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employers`
 --
 
-INSERT INTO `employers` (`employerId`, `employerName`, `employerDescription`, `employerLocation`, `employerType`, `employerSponsor`) VALUES
-(1, 'McDonald\'s', 'We got frozen beef.', '332 St John St Portland ME 04102', 'Business', 'Yes'),
-(2, 'Walmart', 'Sponsored by Lil Pump, Gucci for only the best clothing.', '500 Gallery Blvd Scarborough ME 04074', 'Business', ''),
-(3, 'Subway', 'I like their sandwiches.', '498 Congress St Portland ME 04101', 'Business', 'Yes'),
-(4, 'Wendy\'s', 'Funny prank we don\'t even have any frozen beef.', '50 Market St South Portland ME 04106', 'Business', 'Yes');
+INSERT INTO `employers` (`employerId`, `employerName`, `employerDescription`, `employerLocation`, `employerType`, `employerSponsor`, `Picture`, `test`) VALUES
+(1, 'McDonald\'s', 'We got frozen beef.', '332 St John St Portland ME 04102', 'Business', 'Yes', 'mcdonalds.png', 0),
+(2, 'Walmart', 'Sponsored by Lil Pump, Gucci for only the best clothing.', '500 Gallery Blvd Scarborough ME 04074', 'Business', '', 'walmart.png', 0),
+(3, 'Subway', 'I like their sandwiches.', '498 Congress St Portland ME 04101', 'Business', 'Yes', 'subway.png', 0),
+(4, 'Wendy\'s', 'Funny prank we don\'t even have any frozen beef.', '50 Market St South Portland ME 04106', 'Business', 'Yes', 'wendys.png', 0);
 
 -- --------------------------------------------------------
 
@@ -98,7 +92,12 @@ INSERT INTO `users` (`userId`, `userEmail`, `userToken`, `userFirst`, `userLast`
 (26, 'randomemail@gmail.com', '', 'random', 'person', ''),
 (27, 'burnmytoes9@gmail.com', '', 'Alexander', 'Carter', '9c56df6c8060490abd2c6859348c9746'),
 (28, 'nikowoodhouse@gmail.com', '', 'Niko', 'Woodhouse', 'ee26e7bd21ad86835eb597731e6cc963'),
-(29, 'urmom@gmail.com', '', 'Conner', 'Slaughter Heynen', '4fe2dae188944860765d4879c508ec63');
+(29, 'urmom@gmail.com', '', 'Conner', 'Slaughter Heynen', '4fe2dae188944860765d4879c508ec63'),
+(30, 'rob.korobkin@gmail.com', 'm%QBs86OWe', 'Rob', 'Korobkin', '56bcb005f0e5c4a9da332593c2af0689'),
+(31, 'rob.korobk.in@gmail.com', '', 'Rob', 'Korobkin', '1a912ca20fb31005b507b92247b881f6'),
+(32, 'rob.kor.ob.kin@gmail.com', '', 'Rob', 'Korobkin', '1a912ca20fb31005b507b92247b881f6'),
+(33, 'rob.kokin@gmail.com', '', 'Rob', 'Korobkin', '1a912ca20fb31005b507b92247b881f6'),
+(34, 'rob.kkin@gmail.com', '', 'R', 'K', '912ec803b2ce49e4a541068d495ab570');
 
 --
 -- Indexes for dumped tables
@@ -131,17 +130,15 @@ ALTER TABLE `users`
 --
 ALTER TABLE `employers`
   MODIFY `employerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
   MODIFY `jobId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
